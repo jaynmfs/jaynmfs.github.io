49d9b6d764d68c8f9eca9d7ca46b97338db5f216
JaynMfs [ Jannarong Panpetch ] <jane_marfaresys@outlook.com>
2025-03-25 22:14:09 +0700

    ✨ feat: add simple UI manipulate vanilla JS

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,1 +1,38 @@
 console.log('Welcome to StepNone.')
+
+var starYear = 2019;
+var startYearTimeout = 1000;
+var nicknameColor = 'red'
+var nicknameTimeout = 2000;
+
+function attachExperienceYear(startYear = 2020) {
+    let currentYear = new Date().getFullYear();
+    let experienceYear = currentYear - startYear;
+    let expYearSpans = document.getElementsByClassName('exp_year');
+
+    if (!expYearSpans.length) {
+        return
+    }
+
+    for (let i = 0; i < expYearSpans.length; i++) {
+        expYearSpans[i].textContent = '...' ;
+
+        setTimeout(() => {
+            expYearSpans[i].textContent = experienceYear;
+        }, startYearTimeout);
+    }
+}
+
+attachExperienceYear(starYear);
+
+function changeNicknameColor(color = 'red') {
+    let nickname = document.getElementById('nickname');
+
+    if (nickname) {
+        setTimeout(() => {
+            nickname.style.color = color;
+        }, nicknameTimeout);
+    }
+}
+
+changeNicknameColor();
